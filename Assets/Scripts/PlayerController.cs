e771966cf7408159f1e70c601a4eebcceca79a19
AceuptheSleeve <Kerhold23@gmail.com>
2023-10-05 14:10:21 -0400

    Camera Movement
    Working Camera Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector2 mousePos = Input.mousePosition;
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //Movement
+        if (horizontalInput != 0)
+        {
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * 8);
+        }
+
+        if (verticalInput != 0)
+        {
+            transform.Translate(Vector3.up * verticalInput * Time.deltaTime * 8);
+        }
+    }
+}
