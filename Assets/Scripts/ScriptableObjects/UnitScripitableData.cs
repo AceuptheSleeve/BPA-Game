973673ae2848aa395f4311bcdde9f3792d74c607
AceuptheSleeve <Kerhold23@gmail.com>
2023-10-10 13:52:41 -0400

    Collision needs work
    Maybe make the sphere collider the attack radius

--- Assets/Scripts/ScriptableObjects/UnitScripitableData.cs
+++ Assets/Scripts/ScriptableObjects/UnitScripitableData.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "UnitStats", menuName = "new UnitData", order = 1)]
 public class UnitStats : ScriptableObject
 {
-    public int hp, xp;
-    public float damage, attackRate, attackRange, speed;
-    public bool melee;
-    public GameObject model, projectilePrefab;
+    public int totalHP;
+    public float damage, attackRate, attackRange, speed, spawnTime, projectileSpeed;
+    public GameObject projectilePrefab;
 }
--- Assets/Scripts/UnitScript.cs
+++ Assets/Scripts/UnitScript.cs
@@ -4,19 +4,27 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
 
 public class UnitScript : MonoBehaviour
 {
     public UnitStats stats;
-    public AudioClip[] audioClips;
-    public TilemapCollider2D collider;
+    public int currentHP;//, XP; Leveling System?
+    public float nextAttackTime = 0;
+    //public AudioClip[] audioClips; 
+    public TilemapCollider2D tilemapCollider;
+    public BoxCollider2D hitBox;
+    public GameObject unitRange;
     Vector2 newPos = new Vector2();
 
     // Start is called before the first frame update
     void Start()
     {
-        collider = GetComponent<TilemapCollider2D>();
+        tilemapCollider = GetComponent<TilemapCollider2D>();
+        hitBox = GetComponent<BoxCollider2D>();
+        unitRange.transform.localScale = unitRange.transform.localScale * stats.attackRange;
         newPos = transform.position;
+        currentHP = stats.totalHP;
     }
 
     // Update is called once per frame
@@ -28,6 +36,41 @@ public class UnitScript : MonoBehaviour
         {
             newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Debug.Log("Moving " + gameObject.name + " to " + newPos);
+        }
+
+
+    }
+
+    void Attack(GameObject enemy)
+    {
+        GameObject Projectile = stats.projectilePrefab;
+        Instantiate(Projectile);
+        Projectile.transform.rotation = enemy.transform.rotation;
+        Projectile.transform.position = Vector2.MoveTowards(transform.position, enemy.transform.position, stats.projectileSpeed * Time.deltaTime);   
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHP -= amount;
+
+        if (currentHP <= 0)
+        {
+            unitRange.gameObject.SetActive(false);
+            Debug.Log(gameObject.name + " is dead!");
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            if (Time.time >= nextAttackTime)
+            {
+                Attack(collision.gameObject);
+                Debug.Log(gameObject.name + " is attacking " + collision.gameObject.name);
+                nextAttackTime = Time.time + 1f / stats.attackRate;
+            }
         }
     }
 }
