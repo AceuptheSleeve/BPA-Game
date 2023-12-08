266752e8db20770832416c43d8a16d792503cb5c
Not-So-Human <carsonminz@gmail.com>
2023-12-08 13:28:36 -0500

    Random Unit Spawn
    Units spawn in a random location inside a rectangle. Need the HQ to be a polygon hitbox instead of rectangle to make it so units don't spawn inside HQ.

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -5,15 +5,22 @@ using UnityEngine.UI;
 using System;
 using System.Reflection;
 using Unity.Mathematics;
+using Random = UnityEngine.Random;
 
 public class ButtonScript : MonoBehaviour
 {
     public GameManager gameManager;
+    public GameObject hq;
+    public Vector2 hqPos;
+    public float xPos;
+    public float yPos;
     public int unitIndex;
 
     void Start()
     {
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        hq = GameObject.Find("HQ");
+        hqPos = hq.transform.position;
     }
 
     public void OnButtonPress()
@@ -23,6 +30,7 @@ public class ButtonScript : MonoBehaviour
 
     void SpawnUnit(int index)
     {
-        Instantiate(gameManager.spawnCatalog[index], new Vector2(), new Quaternion());
+        Vector2 randomPos = hqPos + new Vector2(Random.Range(xPos, -xPos), Random.Range(yPos, -yPos));
+        Instantiate(gameManager.spawnCatalog[index], randomPos, new Quaternion());
     }
 }
